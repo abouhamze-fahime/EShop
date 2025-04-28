ecebb61507d2dafa1594c563d92f8671bd1a1229
fahime abouhamze <abouhamze.fahime@gmail.com>
2025-04-28 22:54:00 +0330

    g

---
+++ Persistence/Migrations/20250428191218_first1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class first1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_orderSummaries",
+                table: "orderSummaries");
+
+            migrationBuilder.RenameTable(
+                name: "orderSummaries",
+                newName: "OrderSummaries");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OrderSummaries",
+                table: "OrderSummaries",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OrderSummaries",
+                table: "OrderSummaries");
+
+            migrationBuilder.RenameTable(
+                name: "OrderSummaries",
+                newName: "orderSummaries");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_orderSummaries",
+                table: "orderSummaries",
+                column: "Id");
+        }
+    }
+}
---
+++ Web.API/EndPoints/Orders.cs
@@ -0,0 +1,29 @@
+﻿using Application2.Orders.Create;
+using Application2.Orders.GetOrder.GetOrderSummary;
+using Carter;
+using MediatR;
+
+namespace Web.API.EndPoints
+{
+    public class Orders : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("orders", async (Guid customerId, ISender sender) =>
+            {
+                var command = new CreateOrderCommand(customerId);
+                await sender.Send(command);
+                return Results.Ok();
+            });
+
+            app.MapGet("orders/{id}/summary", async (Guid id, ISender sender) =>
+                {
+
+                    var query = new GetOrderSummaryQuery(id);
+                    return Results.Ok(await sender.Send(query));    
+
+
+                });
+        }
+    }
+}
